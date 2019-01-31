1984498af4920e06e604914426aec0d8b021bf56
iyinraphael <iyinraphael@gmail.com>
2019-01-31 13:50:40 -0800

    implemented NSoperation for dataTask

---
+++ Astronomy/Operations/FetchPhotoOperation.swift
@@ -0,0 +1,41 @@
+//
+//  FetchPhotoOperation.swift
+//  Astronomy
+//
+//  Created by Iyin Raphael on 1/31/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class FetchPhotoOperation: ConcurrentOperation {
+    
+    var dataTask: URLSessionDataTask?
+    var photoReference: MarsPhotoReference
+    var imageData: Data?
+    
+    init(photoReference: MarsPhotoReference){
+        self.photoReference = photoReference
+    }
+    
+    override func start() {
+        self.state = .isExecuting
+        let dataTasksmall = URLSession.shared.dataTask(with: photoReference.imageURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Error occured iin url \(error)")
+                return
+            }
+            guard let data = data else {return}
+            self.imageData = data
+            self.state = .isFinished
+        }
+        dataTask = dataTasksmall
+        dataTask?.resume()
+    }
+    
+    override func cancel() {
+        dataTask?.cancel()
+    }
+    
+    
+}
